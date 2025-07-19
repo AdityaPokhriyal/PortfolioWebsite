9d289dce154fabe1c5762389886f6bfd20800a50
AdityaPokhriyal <62840195+AdityaPokhriyal@users.noreply.github.com>
2025-07-19 15:22:07 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer style={{ textAlign: 'center', padding: '1rem', background: '#2c3e50', color: 'white' }}>
+    <p>© 2025 Aditya Pokhriyal</p>
+  </footer>
+);
+
+export default Footer;
