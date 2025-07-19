3c23c46d1257cf5964f08dab1fdfe95fea08f399
AdityaPokhriyal <62840195+AdityaPokhriyal@users.noreply.github.com>
2025-07-19 15:19:49 +0530

    Create Skills.js

---
+++ src/components/Skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Skills = () => (
+  <section id="skills">
+    <h2>Skills</h2>
+    <ul>
+      <li>HTML5</li>
+      <li>CSS3</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Python</li>
+      <li>FastAPI</li>
+    </ul>
+  </section>
+);
