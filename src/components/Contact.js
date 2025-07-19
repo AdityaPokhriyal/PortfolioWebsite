4077762cd4bf8bb2240bf977879cc50fbfba5b19
AdityaPokhriyal <62840195+AdityaPokhriyal@users.noreply.github.com>
2025-07-19 15:21:48 +0530

    Create Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact">
+    <h2>Contact Me</h2>
+    <p>Email: aditya@example.com</p>
+    <p>LinkedIn: <a href="https://www.linkedin.com/in/aditya-pokhriyal-b2a4531b7" target="_blank">aditya</a></p>
+  </section>
+);
+
+export default Contact;
